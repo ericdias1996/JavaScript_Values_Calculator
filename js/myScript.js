137b9b185564c0958ac9b57814607345a5731004
ericdias96 <77805871+ericdias1996@users.noreply.github.com>
2021-08-04 13:40:38 +0100

    js implementation

--- js/myScript.js
+++ js/myScript.js
@@ -1,29 +1,30 @@
-class User {
-    constructor(firstname, midname, lastname, credit) {
-        this.firstname = firstname;
-        this.midname = midname;
-        this.lastname = lastname;
-        this.credit = credit;
-    }
-    getFullName() {
-        let fullName = `${this.firstname} ${this.midname} ${this.lastname} is my full name`;
-        return fullName;
-    }
-    editName(newName) {
-        const name = newName.split(' ');
-        this.firstname = name[0];
-        this.lastname = name[1];
-    }
-    getMidName() {
-        let midName = `My middle name is ${this.midname}`;
-        return midName;
-    }
+const countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+var slider = document.getElementById("myRange");
+var output = document.getElementById("value");
 
+const x = setInterval(function () {
+
+  const now = new Date().getTime();
+  const distance = countDownDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const secs = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countdownTimer").innerHTML = days + " : " + hours + " : "
+    + mins + " : " + secs;
+
+  if (distance < 0) {
+    document.getElementById("countdownTimer").innerHTML = days + " : " + hours + " : "
+      + mins + " : " + secs;
+    /* clearInterval(x);
+    document.getElementById("countdownTimer").innerHTML = "EXPIRED"; */
+  }
+}, 1000);
+
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+  output.innerHTML = this.value;
 }
-
-const john = new User("Manel", "veríssimo", "Fonseca", 45);
-console.log(john.getFullName());
-john.editName('Manuel Fodasse');
-console.log(john.getFullName());
-john.getMidName('Verissimo');
-console.log(john.getMidName());
